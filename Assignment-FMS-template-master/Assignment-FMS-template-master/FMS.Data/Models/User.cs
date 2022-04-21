b01bd52d8c76d429a9b0258aa2586a7b84625d9c
cbrowne899 <cbrowne899@hotmail.co.uk>
2022-04-22 00:30:59 +0100

    commit

---
+++ Assignment-FMS-template-master/Assignment-FMS-template-master/FMS.Data/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace FMS.Data.Models {
+    
+    public enum Role { admin, manager, guest }
+
+    public class User {
+        public int Id { get; set; }
+        //user ID
+        public string Name { get; set; }
+        //user name
+        public string Email { get; set; }
+        //user email
+        public string Password { get; set; }
+        //user password
+        public Role Role { get; set; }
+        //user role
+
+        
+    }
+}
